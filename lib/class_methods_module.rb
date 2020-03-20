2678149d1c044870ad4b5789cd7849c2b9646296
Terrence Bell <terrencekbell@gmail.com>
2020-03-20 20:46:23 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,9 +3,11 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
-  #class Dancer 
-   # extend MetaDancing
-#  end 
-  
+  class Dancer 
+    extend MetaDancing
+ end 
+  class Kid 
+    extend MetaDancing
+  end
   
 end
