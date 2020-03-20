c06c7d768eb16792d9ec51926570bb87a6968832
Terrence Bell <terrencekbell@gmail.com>
2020-03-20 21:12:22 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,8 +2,14 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  extend FancyDance::ClassMethods
+ include Dance
+  extend MetaDancing
   include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 attr_accessor :name
  
   def initialize(name)
